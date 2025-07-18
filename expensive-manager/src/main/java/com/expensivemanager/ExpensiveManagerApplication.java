da06e398cf28f9bf81acb0ce066bf8dd3b2a7eb2
myDesk262 <learnersdesk262@google.com>
2025-07-18 22:07:27 +0530

    Initialize Spring Boot project with dependencies

---
+++ expensive-manager/src/main/java/com/expensivemanager/ExpensiveManagerApplication.java
@@ -0,0 +1,13 @@
+package com.expensivemanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExpensiveManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExpensiveManagerApplication.class, args);
+	}
+
+}
---
+++ expensive-manager/src/test/java/com/expensivemanager/ExpensiveManagerApplicationTest.java
@@ -0,0 +1,13 @@
+package com.expensivemanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExpensiveManagerApplicationTest {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
