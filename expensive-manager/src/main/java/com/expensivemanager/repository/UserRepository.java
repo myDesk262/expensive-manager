82e8dfd6a31fb35daf52064a94e8953feda1b2b7
myDesk262 <learnersdesk262@google.com>
2025-07-18 23:48:23 +0530

    User Repository initial

---
+++ expensive-manager/src/main/java/com/expensivemanager/repository/UserRepository.java
@@ -0,0 +1,14 @@
+package com.expensivemanager.repository;
+
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.expensivemanager.model.User;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findByUsername(String username);
+    Optional<User> findByEmail(String email);
+}
